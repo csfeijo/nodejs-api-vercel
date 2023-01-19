043e9dea3a1639e2393bfbd6788c67fa8e107b86
Cicero, Feijo <cicero.feijo@gmail.com>
2023-01-19 17:33:47 -0300

    Added /repos route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -13,19 +14,33 @@ app.get('/', (req, res) => {
   res.send('Bombando na Azion')
 })
 
-app.get('/repos', (req, res) => {
-  
-  res.write('DATA ')
+app.get('/variables', (req, res) => {  
+  res.write('ENV VARIABLES ')
 
-  
   try {
     res.write(process.env.NAME)
   } catch (e) {
     res.write('VAR - Not available')
   }
-
   res.end()
 })
+
+
+app.get('/repos/:user', (req, res) => {
+  const { user } = req.params
+  const base = 'https://api.github.com'
+
+ 
+  const hostname = `${base}/users/${user}/repos`;
+   
+  axios.get(hostname)
+    .then(response => {
+      //console.log(response.data)
+      res.json(response.data)
+    })
+    .catch(error => console.log(error))
+})
+
 
 // Export the Express API
 module.exports = app
